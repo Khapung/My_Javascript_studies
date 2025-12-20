897db96636838214bf761e3ac235e68f69c5a457
Sam Khapung <samlimroses@gmail.com>
2025-12-20 22:14:49 +0000

    chapter 3 lesson 21 completed

--- 3. Build a Blackjack game/21. Intro to arrays/index.js
+++ 3. Build a Blackjack game/21. Intro to arrays/index.js
@@ -6,6 +6,8 @@ let featuredPosts = [
     "I've just relaunched my portfolio"
 ]
 
-// Create an array that lists your i.e. experience, education, licenses, skills or similar
+// Create an array that lists your i.e. experience, 
+// education, licenses, skills or similar
 // The items of the array should be strings
 
+let experience = ["System admninistration", "Network Engineer", "Cyber Security analyst"]
