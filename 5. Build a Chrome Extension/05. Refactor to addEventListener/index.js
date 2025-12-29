1cbbd0f290ce2ffc178787dd167ff0b06d54ea4a
Sam Khapung <samlimroses@gmail.com>
2025-12-29 23:36:31 +0000

    chapter 5 lesson 5 completed

--- 5. Build a Chrome Extension/05. Refactor to addEventListener/index.js
+++ 5. Build a Chrome Extension/05. Refactor to addEventListener/index.js
@@ -1,6 +1,11 @@
-function saveLead() {
-    console.log("Button clicked!")
-}
+let messageEl = document.getElementById("message-el")
+
+let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function() {
+    messageEl.textContent = "Button clicked! from addEventListener"
+})
+
+
 
 
 
