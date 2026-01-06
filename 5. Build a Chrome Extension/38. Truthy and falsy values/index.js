77d51cecd4948148535ce570eeafdc73e3985a26
Sam Khapung <samlimroses@gmail.com>
2026-01-06 23:33:50 +0000

    Refactored chapter 5 lesson 38

--- 5. Build a Chrome Extension/38. Truthy and falsy values/index.js
+++ 5. Build a Chrome Extension/38. Truthy and falsy values/index.js
@@ -2,5 +2,5 @@ console.log(  Boolean("")   ) // false
 console.log(  Boolean("0")  ) // true
 console.log(  Boolean(100)  ) // true
 console.log(  Boolean(null) ) // false
-console.log(  Boolean([0])  ) // undefined
+console.log(  Boolean([0])  ) // true
 console.log(  Boolean(-0)   ) // false
