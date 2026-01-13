50e75209f231c84dcbee8ad52558ffaf041ad065
Sam Khapung <samlimroses@gmail.com>
2026-01-13 00:33:21 +0000

    chapter 5 lesson 52 completed

--- 6. Practice time - part 3/03. save to localStorage/index.js
+++ 6. Practice time - part 3/03. save to localStorage/index.js
@@ -1,3 +1,8 @@
 // Save a value to localStorage
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+
+localStorage.setItem('myValue', 'Hello, World!')
+
+let savedValue = localStorage.getItem('myValue')
+console.log(savedValue)
