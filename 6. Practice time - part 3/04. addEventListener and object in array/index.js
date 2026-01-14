a89fcc57f392a189e7a844b4832497d8de0e20c8
Sam Khapung <samlimroses@gmail.com>
2026-01-14 19:00:00 +0000

    chapter 6 lesson 3 completed

--- 6. Practice time - part 3/04. addEventListener and object in array/index.js
+++ 6. Practice time - part 3/04. addEventListener and object in array/index.js
@@ -12,3 +12,10 @@ let data = [
 // Fetch the button from the DOM, store it in a variable
 // Use addEventListener() to listen for button clicks
 // Log Jane's score when the button is clicked (via data)
+
+let button = document.getElementById("buttonId")
+let scoreParagraph = document.getElementById("scoreid")
+
+button.addEventListener("click", function() {
+    scoreParagraph.textContent = `Jane's score is: ${data[0].score}`    
+})  
